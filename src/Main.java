ad014367887c32e40ec0e21367d7580f4ef89061
StarFleet1334 <86927329+StarFleet1334@users.noreply.github.com>
2022-10-09 09:25:11 +0400

    Added Jupiter

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello From Mars");
         System.out.println("Master");
+        System.out.println("Jupiter");
     }
 }
