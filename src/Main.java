d6fcd465b122e7065bbd38528a12f3a6e27a46e0
StarFleet1334 <86927329+StarFleet1334@users.noreply.github.com>
2022-10-09 14:34:11 +0400

    J

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
         System.out.println("Hello From Mars");
         System.out.println("Master");
         System.out.println("Jupiter");
+
+
+        System.out.println("Jupiter");
     }
 }
